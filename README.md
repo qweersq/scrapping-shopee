41792faa5438fd82abc03acc705810e9e1f5a4f2
Heryan DP <haxornesia@gmail.com>
2022-08-03 15:17:53 +0700

    Add files via upload
    Penambahan pencarian dan grabbing seller

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-Shopee Product Scrapper v2.0
+Shopee Product Scrapper v3.0
 
 !['ss'](https://user-images.githubusercontent.com/10804727/182350115-a38e56d7-c7f6-40e3-b8dc-02a7c7671d14.png)
-
-Req:
-- python3
-- requests
-- progressbar,progressbar2
 
 Cara menggunakan:
 1. py shopee.py
 2. Masukkan username sellernya
 
-Link req:
+Req dan Link req:
 - https://www.python.org/downloads/
-- pip install requests bs4 progressbar progressbar2
+- pip install requests bs4 progressbar progressbar2 colorama
--- shopee.py
+++ shopee.py
@@ -3,9 +3,12 @@
 # https://github.com/heryandp
 
 from progressbar import progressbar
+from colorama import Fore, Back, Style,init
 import requests as req
 import urllib.parse as encode
-import os,glob, json,time,csv
+import os,glob, json,time,csv,datetime
+
+init(autoreset=True)
 
 # Base Url
 base_url = "https://shopee.co.id/"
@@ -14,11 +17,107 @@ ua = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.3
 class shopee_shop_detil():
     def detil(shopid):
         try:
-            url_detil = base_url+"api/v4/recommend/get_shop_detail/?shopId="+str(shopid)
+            url_detil = base_url+"api/v4/shop/get_shop_detail?shopid="+str(shopid)
             grab_detil = req.get(url_detil,headers=ua,timeout=3000).json()
-            return grab_detil['data']['account']['username']
+            return grab_detil
         except:
             return "error"
+
+class shopee_seller():
+    def __init__(self,keyword):
+        self.headerbrowser = ua
+        self.katakunci = encode.quote(keyword)
+        self.fn = str(keyword).replace(" ","_").replace(".","_")
+        try:
+            self.grab_seller()
+        except Exception as e:
+            print(e)
+
+    def grab_seller(self):
+        sess = req.Session()
+        self.url_key = base_url+"api/v4/search/search_user?keyword="+self.katakunci+"&limit=1&offset=0&page=search_user&with_search_cover=true"
+        get_key = req.get(self.url_key,headers=self.headerbrowser,timeout=3000).json()
+        get_sess = sess.cookies
+        cookies_sess = get_sess.get_dict()
+        print("=== GRABBING SELLER ===")
+        print("[+] Hapus file lama ...")
+        if not os.path.exists("data"):
+            os.makedirs("data")
+        for filename in glob.glob("data/seller_"+self.fn+"_shopee*.json"):
+            os.remove(filename)
+        for filename in glob.glob("data/seller_"+self.fn+"_shopee*.csv"):
+            os.remove(filename)
+        print("[+] Mulai download list seller ...")
+        print("\x1B[3m" +"(delay 5 detik untuk menghindari anti-spam!)"+"\x1B[0m")
+        a = 0
+        b = 1
+        while(True):
+            url_seller = base_url+"api/v4/search/search_user?keyword="+self.katakunci+"&limit=100&offset="+str(a)+"&page=search_user&with_search_cover=true"
+            cek_seller = req.get(url_seller,headers=self.headerbrowser,cookies=cookies_sess).json()
+            if (not cek_seller["data"]) or (not cek_seller["data"]["users"]):
+                # print(cek_seller)
+                break
+            else:
+                print("-> download halaman "+ str(b) +" ("+ str(len(cek_seller["data"]["users"])) +")")
+                # print(url_produk)
+                with open("data/seller_"+self.fn+"_shopee_"+str(b)+".json", 'w') as json_file:
+                    json.dump(cek_seller["data"]["users"], json_file)
+                a = a + 100
+                b = b + 1
+                time.sleep(5)
+        # merging json
+        print("[+] Merging data seller ...")
+        data = []
+        for f in progressbar(glob.glob("data/seller_"+self.fn+"_shopee_*.json")):
+            with open(f,) as infile:
+                data.extend(json.load(infile))
+        with open("data/seller_"+self.fn+"_shopee_all.json",'w') as outfile:
+            json.dump(data, outfile)
+        # create csv
+        print("[+] Membuat csv list seller ...")
+        print("\x1B[3m" +"(sabar ya... grabbing detil list sellernya lumayan lama)"+"\x1B[0m")
+        f_data = []
+        f = open("data/seller_"+self.fn+"_shopee_all.json")
+        f_read = json.load(f)
+        for i in progressbar(f_read):
+            # get detil seller
+            data = shopee_shop_detil.detil(i['shopid'])
+            try:
+                desc = data['data']['description'].replace("\n","<br></br>").replace('"',"")
+            except:
+                desc = ""
+            t_aktif = datetime.datetime.fromtimestamp(data['data']['last_active_time']).strftime('%d-%m-%Y %H:%M:%S')
+            f_data.append([
+                data['data']['userid'],
+                data['data']['shopid'],
+                data['data']['account']['username'],
+                data['data']['name'],
+                data['data']['country'],
+                data['data']['shop_location'],
+                data['data']['is_shopee_verified'],
+                data['data']['is_official_shop'],
+                data['data']['rating_normal'],
+                data['data']['rating_bad'],
+                data['data']['rating_good'],
+                data['data']['cancellation_rate'],
+                data['data']['shop_location'],
+                data['data']['rating_star'],
+                data['data']['item_count'],
+                data['data']['follower_count'],
+                data['data']['response_rate'],
+                data['data']['response_time'],
+                data['data']['address'],
+                t_aktif,
+                desc
+                ])
+            time.sleep(0.2)
+        f_header = ['userid','shopid','username','nama_toko','negara','lokasi_toko','is_shopee_verified','is_official_shop','rating_normal','rating_bad','rating_good','cancelation_rate','total_rating','total_produk','total_follower','kec_respon_%','wkt_respon_detik','alamat','terakhir_aktif','deskripsi']
+        with open('seller_'+str(self.fn)+'_shopee.csv', 'w',newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(f_header)
+            writer.writerows(f_data)
+        f.close()
+        print("done! seller_"+str(self.fn)+"_shopee.csv")
 class shopee_keyword():
     def __init__(self,keyword,lokasi):
         self.headerbrowser = ua
@@ -99,7 +198,7 @@ class shopee_keyword():
                 pmaxbd = i['item_basic']['price_max_before_discount']/100000
             f_data.append([
                 i['item_basic']['shopid'],
-                namatoko,
+                namatoko['data']['account']['username'],
                 i['item_basic']['shop_location'],
                 i['item_basic']['itemid'],
                 i['item_basic']['name'],
@@ -235,23 +334,23 @@ class shopee():
         f.close()
         print("done! "+str(self.idseller)+"_shopee.csv")
 
-print("[ SHOPEE-PRODUCT-GRABBER v2.0 by heryan ]")
+print(Fore.GREEN + "[ SHOPEE-PRODUCT-GRABBER v3.0 by heryan ]")
 print(" _____ _                           ")
 print("/  ___| |                          ")
 print("\ `--.| |__   ___  _ __   ___  ___ ")
 print(" `--. \ '_ \ / _ \| '_ \ / _ \/ _ /")
-print("/\__/ / | | | (_) | |_) |  __/  __/   v.2.0")
+print("/\__/ / | | | (_) | |_) |  __/  __/   v.3.0")
 print("\____/|_| |_|\___/| .__/ \___|\___|")
 print("                  | |              ")
 print("                  |_|              ")
-print("[+] https://github.com/heryandp/shopee-product-scrap")
-
+print(Fore.GREEN + "[+] https://github.com/heryandp/shopee-product-scrap")
 while(True):
     print("===========================")
     print("[+] Pilih metode grab/scrap:")
     print("1) Username toko (download produk per toko)")
     print("2) Keyword + Lokasi/Daerah (download produk terlaris)")
-    print("3) Keluar")
+    print("3) Keyword toko/username (download list toko)")
+    print("4) Keluar")
     choice = input("Ketik nomor :")
     if choice=="1":
         sname = input("[+] Masukkan username seller: https://shopee.co.id/")        
@@ -263,6 +362,10 @@ while(True):
         key = shopee_keyword(keyword,lokasi)
         break
     elif choice=="3":
+        keyword = input("[+] Masukkan keyword toko/username (mis. official) :")
+        key = shopee_seller(keyword)
+        break
+    elif choice=="4":
         exit()
     else:
-        print("Oops! Ketik nomor 1,2,atau 3 :")
+        print("Oops! Ketik nomor 1 s.d. 4 :")
